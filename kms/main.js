08b8c80d659496b4bff00f3f7699f1eab87391cb
Greg McClement <dev@thinktelligence.com>
2024-04-16 21:41:19 -0700

    sorted

--- kms/main.js
+++ kms/main.js
@@ -1,89 +1,89 @@
 const tpm = require('./common/runtime').theprogrammablemind
-const currency = require('./common/currency')
-const numbers = require('./common/numbers')
-const people = require('./common/people')
-const time = require('./common/time')
+const animals = require('./common/animals')
 const avatar = require('./common/avatar')
+const characters = require('./common/characters')
 const comparable = require('./common/comparable')
 const countable = require('./common/countable')
-const help = require('./common/help')
-const meta = require('./common/meta')
-const hierarchy = require('./common/hierarchy')
-const ordering = require('./common/ordering')
-const properties = require('./common/properties')
-const dimensionTemplate = require('./common/dimensionTemplate')
-const formulasTemplate = require('./common/formulasTemplate')
-const mathTemplate = require('./common/mathTemplate')
-const pipboyTemplate = require('./common/pipboyTemplate')
-const dialogues = require('./common/dialogues')
-const gdefaults = require('./common/gdefaults')
-const pos = require('./common/pos')
-const punctuation = require('./common/punctuation')
-const stm = require('./common/stm')
-const ui = require('./common/ui')
-const math = require('./common/math')
-const formulas = require('./common/formulas')
-const dimension = require('./common/dimension')
-const length = require('./common/length')
-const pressure = require('./common/pressure')
-const temperature = require('./common/temperature')
-const weight = require('./common/weight')
-const animals = require('./common/animals')
-const foods = require('./common/foods')
-const javascript = require('./common/javascript')
-const scorekeeper = require('./common/scorekeeper')
-const reports = require('./common/reports')
-const tell = require('./common/tell')
-const pipboy = require('./common/pipboy')
-const fastfood = require('./common/fastfood')
-const characters = require('./common/characters')
 const crew = require('./common/crew')
-const stgame = require('./common/stgame')
+const currency = require('./common/currency')
+const dialogues = require('./common/dialogues')
+const dimension = require('./common/dimension')
+const dimensionTemplate = require('./common/dimensionTemplate')
+const fastfood = require('./common/fastfood')
+const foods = require('./common/foods')
+const formulas = require('./common/formulas')
+const formulasTemplate = require('./common/formulasTemplate')
+const gdefaults = require('./common/gdefaults')
+const help = require('./common/help')
+const hierarchy = require('./common/hierarchy')
+const javascript = require('./common/javascript')
 const kirk = require('./common/kirk')
+const length = require('./common/length')
+const math = require('./common/math')
+const mathTemplate = require('./common/mathTemplate')
+const meta = require('./common/meta')
+const numbers = require('./common/numbers')
+const ordering = require('./common/ordering')
+const people = require('./common/people')
+const pipboy = require('./common/pipboy')
+const pipboyTemplate = require('./common/pipboyTemplate')
+const pos = require('./common/pos')
+const pressure = require('./common/pressure')
+const properties = require('./common/properties')
+const punctuation = require('./common/punctuation')
+const reports = require('./common/reports')
+const scorekeeper = require('./common/scorekeeper')
 const spock = require('./common/spock')
+const stgame = require('./common/stgame')
+const stm = require('./common/stm')
+const tell = require('./common/tell')
+const temperature = require('./common/temperature')
+const time = require('./common/time')
+const ui = require('./common/ui')
+const weight = require('./common/weight')
 
 module.exports = {
-Config: tpm.Config,
-currency,
-numbers,
-people,
-time,
-avatar,
-comparable,
-countable,
-help,
-meta,
-hierarchy,
-ordering,
-properties,
-dimensionTemplate,
-formulasTemplate,
-mathTemplate,
-pipboyTemplate,
-dialogues,
-gdefaults,
-pos,
-punctuation,
-stm,
-ui,
-math,
-formulas,
-dimension,
-length,
-pressure,
-temperature,
-weight,
-animals,
-foods,
-javascript,
-scorekeeper,
-reports,
-tell,
-pipboy,
-fastfood,
-characters,
-crew,
-stgame,
-kirk,
-spock,
+  Config: tpm.Config,
+  animals,
+  avatar,
+  characters,
+  comparable,
+  countable,
+  crew,
+  currency,
+  dialogues,
+  dimension,
+  dimensionTemplate,
+  fastfood,
+  foods,
+  formulas,
+  formulasTemplate,
+  gdefaults,
+  help,
+  hierarchy,
+  javascript,
+  kirk,
+  length,
+  math,
+  mathTemplate,
+  meta,
+  numbers,
+  ordering,
+  people,
+  pipboy,
+  pipboyTemplate,
+  pos,
+  pressure,
+  properties,
+  punctuation,
+  reports,
+  scorekeeper,
+  spock,
+  stgame,
+  stm,
+  tell,
+  temperature,
+  time,
+  ui,
+  weight,
 }
