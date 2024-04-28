5a33442a8ed905392a769b56c77e7453f7847745
Greg McClement <dev@thinktelligence.com>
2024-04-28 10:14:06 -0700

    move api to its own file

--- kms/common/dialogues.js
+++ kms/common/dialogues.js
@@ -4,109 +4,11 @@ const gdefaults = require('./gdefaults.js')
 const pos = require('./pos.js')
 const stm = require('./stm.js')
 const _ = require('lodash')
+const { API } = require('./helpers/dialogues')
 const { isMany } = require('./helpers')
 const dialogues_tests = require('./dialogues.test.json')
 const { indent, focus } = require('./helpers')
 const pluralize = require('pluralize')
-
-class API {
-  initialize() {
-  }
-
-  //
-  // duck typing: for operators you want to use here
-  //
-  //   1. Use hierarchy to make them an instance of queryable. For example add hierarchy entry [<myClassId>, 'queryable']
-  //   2. For semantics, if evaluate == true then set the 'value' property of the operator to the value.
-  //   3. Generators will get contexts with 'response: true' set. Used for converting 'your' to 'my' to phrases like 'your car' or 'the car'.
-  //   4. Generators will get contexts with 'instance: true' and value set. For converting values like a date to a string.
-  //
-
-  // used with context sensitive words like 'it', 'that' etc. for example if you have a sentence "create a tank"
-  // then call mentioned with the tank created. Then if one asks 'what is it' the 'it' will be found to be the tank.
-
-  setBrief(value) {
-    this.objects.brief = value
-  }
-
-  getBrief() {
-    return this.objects.brief
-  }
-
-  evaluateToConcept(value, context, log, s) {
-    value.evaluate = { toConcept: true }
-    // const concept = s(value, { debug: { apply: true } }) 
-    const concept = s(value)
-    if (!concept.evalue && !concept.verbatim) {
-      warningNotEvaluated(log, value);
-      concept.evalue = concept.value
-    }
-    delete concept.evaluate
-    return concept
-  }
-
-  setupObjectHierarchy(config, id, { types } = {}) {
-    for (let type of types) {
-      config.addHierarchy(id, type)
-    }
-  }
-
-  // word is for one or many
-  makeObject({config, context, types=[], doPluralize=true} = {}) {
-    /*
-    if (!context.unknown) {
-      return context.value
-    }
-    */
-    const { word, value, number } = context;
-    if (!value) {
-      return
-    }
-    const concept = pluralize.singular(value)
-		if (config.exists(concept)) {
-			return concept
-		}
-
-    // TODO handle the general case
-    const fixUps = (concept) => {
-      if (concept == '*') {
-        return '\\*'
-      }
-      return concept
-    }
-    config.addOperator({ pattern: `([${fixUps(concept)}])`, allowDups: true })
-    config.addBridge({ id: concept, level: 0, bridge: `{ ...next(operator), value: '${concept}' }` , allowDups: true })
-    const addConcept = (word, number) => {
-      config.addWord(word, { id: concept, initial: `{ value: "${concept}", number: "${number}" }` } )
-      const baseTypes = [
-        'theAble',
-        'queryable',
-        'isEdee',
-        'isEder',
-      ];
-
-      const allTypes = new Set(baseTypes.concat(types))
-      this.setupObjectHierarchy(config, concept, {types: allTypes});
-    }
-
-    if (pluralize.isSingular(word)) {
-      addConcept(word, 'one')
-      doPluralize && addConcept(pluralize.plural(word), 'many')
-    } else {
-      doPluralize && addConcept(pluralize.singular(word), 'one')
-      addConcept(word, 'many')
-    }
-
-    // mark greg as an instance?
-    // add a generator for the other one what will ask what is the plural or singluar of known
-    /*
-    if (number == 'many') {
-    } else if (number == 'one') {
-    }
-    */
-    return concept;
-  }
-}
 
 const api = new API()
 
@@ -115,15 +17,6 @@ const warningIsANotImplemented = (log, context) => {
   const match = `({context, hierarchy}) => hierarchy.isA(context.marker, 'is') && context.query && <other conditions as you like>`
   const apply = `({context}) => <do stuff...>; context.evalue = <value>`
   const input = indent(JSON.stringify(context, null, 2), 2)
-  const message = `${description}\nThe semantic would be\n  match: ${match}\n  apply: ${apply}\nThe input context would be:\n${input}\n`
-  log(indent(message, 4))
-}
-
-const warningNotEvaluated = (log, value) => {
-  const description = 'WARNING from Dialogues KM: For semantics, implement an evaluations handler, set "value" property of the operator to the value.'
-  const match = `({context}) => context.marker == '${value.marker}' && context.evaluate && <other conditions as you like>`
-  const apply = `({context}) => <do stuff...>; context.value = <value>`
-  const input = indent(JSON.stringify(value, null, 2), 2)
   const message = `${description}\nThe semantic would be\n  match: ${match}\n  apply: ${apply}\nThe input context would be:\n${input}\n`
   log(indent(message, 4))
 }
---
+++ kms/common/helpers/dialogues.js
@@ -0,0 +1,114 @@
+const pluralize = require('pluralize')
+const { indent, focus } = require('../helpers')
+
+class API {
+  initialize() {
+  }
+
+  warningNotEvaluated(log, value) {
+    const description = 'WARNING from Dialogues KM: For semantics, implement an evaluations handler, set "value" property of the operator to the value.'
+    const match = `({context}) => context.marker == '${value.marker}' && context.evaluate && <other conditions as you like>`
+    const apply = `({context}) => <do stuff...>; context.value = <value>`
+    const input = indent(JSON.stringify(value, null, 2), 2)
+    const message = `${description}\nThe semantic would be\n  match: ${match}\n  apply: ${apply}\nThe input context would be:\n${input}\n`
+    log(indent(message, 4))
+  }
+
+  //
+  // duck typing: for operators you want to use here
+  //
+  //   1. Use hierarchy to make them an instance of queryable. For example add hierarchy entry [<myClassId>, 'queryable']
+  //   2. For semantics, if evaluate == true then set the 'value' property of the operator to the value.
+  //   3. Generators will get contexts with 'response: true' set. Used for converting 'your' to 'my' to phrases like 'your car' or 'the car'.
+  //   4. Generators will get contexts with 'instance: true' and value set. For converting values like a date to a string.
+  //
+
+  // used with context sensitive words like 'it', 'that' etc. for example if you have a sentence "create a tank"
+  // then call mentioned with the tank created. Then if one asks 'what is it' the 'it' will be found to be the tank.
+
+  setBrief(value) {
+    this.objects.brief = value
+  }
+
+  getBrief() {
+    return this.objects.brief
+  }
+
+  evaluateToConcept(value, context, log, s) {
+    value.evaluate = { toConcept: true }
+    // const concept = s(value, { debug: { apply: true } }) 
+    const concept = s(value)
+    if (!concept.evalue && !concept.verbatim) {
+      this.warningNotEvaluated(log, value);
+      concept.evalue = concept.value
+    }
+    delete concept.evaluate
+    return concept
+  }
+
+  setupObjectHierarchy(config, id, { types } = {}) {
+    for (let type of types) {
+      config.addHierarchy(id, type)
+    }
+  }
+
+  // word is for one or many
+  makeObject({config, context, types=[], doPluralize=true} = {}) {
+    /*
+    if (!context.unknown) {
+      return context.value
+    }
+    */
+    const { word, value, number } = context;
+    if (!value) {
+      return
+    }
+    const concept = pluralize.singular(value)
+		if (config.exists(concept)) {
+			return concept
+		}
+
+    // TODO handle the general case
+    const fixUps = (concept) => {
+      if (concept == '*') {
+        return '\\*'
+      }
+      return concept
+    }
+    config.addOperator({ pattern: `([${fixUps(concept)}])`, allowDups: true })
+    config.addBridge({ id: concept, level: 0, bridge: `{ ...next(operator), value: '${concept}' }` , allowDups: true })
+    const addConcept = (word, number) => {
+      config.addWord(word, { id: concept, initial: `{ value: "${concept}", number: "${number}" }` } )
+      const baseTypes = [
+        'theAble',
+        'queryable',
+        'isEdee',
+        'isEder',
+      ];
+
+      const allTypes = new Set(baseTypes.concat(types))
+      this.setupObjectHierarchy(config, concept, {types: allTypes});
+    }
+
+    if (pluralize.isSingular(word)) {
+      addConcept(word, 'one')
+      doPluralize && addConcept(pluralize.plural(word), 'many')
+    } else {
+      doPluralize && addConcept(pluralize.singular(word), 'one')
+      addConcept(word, 'many')
+    }
+
+    // mark greg as an instance?
+    // add a generator for the other one what will ask what is the plural or singluar of known
+    /*
+    if (number == 'many') {
+    } else if (number == 'one') {
+    }
+    */
+    return concept;
+  }
+}
+
+module.exports = {
+  API
+}
