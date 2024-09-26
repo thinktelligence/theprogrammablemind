09f00d672d64fc07d6554e4caf8de404e85df4e5
Greg McClement <greg.mcclement@gmail.com>
2024-09-26 09:24:18 -0700

    set priority

--- kms/common/comparable.js
+++ kms/common/comparable.js
@@ -17,7 +17,6 @@ let config = {
   bridges: [
     { 
       id: "compare", 
-      level: 0, 
       convolution: true, 
       before: ['verb', 'articlePOS'],
       // bridge: "{ ...after, comparison: append(before[0], after[0].comparison), modifiers: append([before[0].marker], after[0].modifiers), [before[0].marker]: before[0] }" 
@@ -28,32 +27,28 @@ let config = {
     { 
       id: "condition", 
       children: ['highest', 'lowest'],
-      level: 0, 
       bridge: "{ ...next(operator) }" 
     },
     { 
       id: "comparable", 
-      level: 0, 
       bridge: "{ ...next(operator) }" 
     },
     { 
       id: "lowest", 
-      level: 0, 
       bridge: "{ ...next(operator) }" 
     },
     { 
       id: "highest", 
-      level: 0, 
       bridge: "{ ...next(operator) }" 
     },
     { 
       id: "ascending", 
-      level: 0, 
+      isA: ['adjective'],
       bridge: "{ ...next(before[0]), ordering: 'ascending', ascending: operator, postModifiers: append(['ascending'], before[0].postModifiers) }" 
     },
     { 
       id: "descending", 
-      level: 0, 
+      isA: ['adjective'],
       bridge: "{ ...next(before[0]), ordering: 'descending', descending: operator, postModifiers: append(['descending'], before[0].postModifiers) }" 
     },
   ],
