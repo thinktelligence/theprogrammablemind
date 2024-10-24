761c1d26cc4d8699864d4969c42d201c38b1301e
Greg McClement <greg.mcclement@gmail.com>
2024-10-24 09:52:25 -0700

    fixs ups

--- kms/common/dialogues.js
+++ kms/common/dialogues.js
@@ -930,8 +930,8 @@ const initializer = ({objects, config, isModule}) => {
       return api('dialogues').toScopedId(context)
     },
     addWords: (id, word, additional) => {
-      for (let props of words(word, additional)) {
-        config.addWord(id, props) 
+      for (let props of words(word, { ...additional, id })) {
+        config.addWord(props.word, props) 
       }
     }
   }))
