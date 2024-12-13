3370c98b2b1a4e507ee0b23fd4051ea3d6f12378
Greg McClement <greg.mcclement@gmail.com>
2024-12-13 09:29:25 -0800

    add integer

--- kms/common/numbers.js
+++ kms/common/numbers.js
@@ -14,37 +14,44 @@ let config = {
   name: 'numbers',
   operators: [
     "([number])",
+    "([integer])",
   ],
   bridges: [
-    { "id": "number", "level": 0, "bridge": "{ instance: false, ...next(operator) }" },
+    { 
+      id: "number", 
+      bridge: "{ instance: false, ...next(operator) }" 
+    },
+    { 
+      id: "integer", 
+      isA: ['number'],
+      bridge: "{ instance: false, ...next(operator) }"
+   },
   ],
-  debug: false,
-  version: '3',
   words: {
     "literals": {
-      "one": [{"id": "number", "initial": "{ value: 1, instance: true }" }],
-      "ones": [{"id": "number", "initial": "{ value: 1, number: 'many', instance: true }" }],
-      "two": [{"id": "number", "initial": "{ value: 2 , instance: true}" }],
-      "twos": [{"id": "number", "initial": "{ value: 2 , number: 'many', instance: true}" }],
-      "three": [{"id": "number", "initial": "{ value: 3, instance: true }" }],
-      "threes": [{"id": "number", "initial": "{ value: 3, number: 'many', instance: true }" }],
-      "four": [{"id": "number", "initial": "{ value: 4, instance: true }" }],
-      "fours": [{"id": "number", "initial": "{ value: 4, number: 'many', instance: true }" }],
-      "five": [{"id": "number", "initial": "{ value: 5, instance: true }" }],
-      "fives": [{"id": "number", "initial": "{ value: 5, number: 'many', instance: true }" }],
-      "six": [{"id": "number", "initial": "{ value: 6, instance: true }" }],
-      "sixes": [{"id": "number", "initial": "{ value: 6, number: 'many', instance: true }" }],
-      "seven": [{"id": "number", "initial": "{ value: 7, instance: true }" }],
-      "sevens": [{"id": "number", "initial": "{ value: 7, number: 'many', instance: true }" }],
-      "eight": [{"id": "number", "initial": "{ value: 8, instance: true }" }],
-      "eights": [{"id": "number", "initial": "{ value: 8, number: 'many', instance: true }" }],
-      "nine": [{"id": "number", "initial": "{ value: 9, instance: true }" }],
-      "nines": [{"id": "number", "initial": "{ value: 9, number: 'many', instance: true }" }],
-      "ten": [{"id": "number", "initial": "{ value: 10, instance: true }" }],
-      "tens": [{"id": "number", "initial": "{ value: 10, number: 'many', instance: true }" }],
+      "one": [{"id": "integer", "initial": "{ value: 1, instance: true }" }],
+      "ones": [{"id": "integer", "initial": "{ value: 1, integer: 'many', instance: true }" }],
+      "two": [{"id": "integer", "initial": "{ value: 2 , instance: true}" }],
+      "twos": [{"id": "integer", "initial": "{ value: 2 , integer: 'many', instance: true}" }],
+      "three": [{"id": "integer", "initial": "{ value: 3, instance: true }" }],
+      "threes": [{"id": "integer", "initial": "{ value: 3, integer: 'many', instance: true }" }],
+      "four": [{"id": "integer", "initial": "{ value: 4, instance: true }" }],
+      "fours": [{"id": "integer", "initial": "{ value: 4, integer: 'many', instance: true }" }],
+      "five": [{"id": "integer", "initial": "{ value: 5, instance: true }" }],
+      "fives": [{"id": "integer", "initial": "{ value: 5, integer: 'many', instance: true }" }],
+      "six": [{"id": "integer", "initial": "{ value: 6, instance: true }" }],
+      "sixes": [{"id": "integer", "initial": "{ value: 6, integer: 'many', instance: true }" }],
+      "seven": [{"id": "integer", "initial": "{ value: 7, instance: true }" }],
+      "sevens": [{"id": "integer", "initial": "{ value: 7, integer: 'many', instance: true }" }],
+      "eight": [{"id": "integer", "initial": "{ value: 8, instance: true }" }],
+      "eights": [{"id": "integer", "initial": "{ value: 8, integer: 'many', instance: true }" }],
+      "nine": [{"id": "integer", "initial": "{ value: 9, instance: true }" }],
+      "nines": [{"id": "integer", "initial": "{ value: 9, integer: 'many', instance: true }" }],
+      "ten": [{"id": "integer", "initial": "{ value: 10, instance: true }" }],
+      "tens": [{"id": "integer", "initial": "{ value: 10, integer: 'many', instance: true }" }],
     },
     patterns: [
-      { "pattern": [{ type: 'digit' }, { repeat: true }], defs: [{id: "number", uuid: '1', initial: "{ value: int(text), instance: true }" }]},
+      { "pattern": [{ type: 'digit' }, { repeat: true }], defs: [{id: "integer", uuid: '1', initial: "{ value: int(text), instance: true }" }]},
       { "pattern": [{ type: 'digit' }, { repeat: true }, '.', { type: 'digit' }, { repeat: true }], defs: [{id: "number", uuid: '1', initial: "{ value: float(text), instance: true }" }]},
       { "pattern": ['.', { type: 'digit' }, { repeat: true }], defs: [{id: "number", uuid: '1', initial: "{ value: float(text), instance: true }" }]},
     ],
@@ -58,7 +65,7 @@ let config = {
   generators: [
     { 
       where: where(),
-      match: ({context}) => context.marker == 'number' && context.leadingZeros && context.value >= 0, 
+      match: ({context, isA}) => isA(context.marker, 'number', { extended: true }) && context.leadingZeros && context.value >= 0, 
       apply: ({context}) => {
         value = `${context.value}` 
         if (value.length < context.length) {
@@ -69,12 +76,12 @@ let config = {
     },
     { 
       where: where(),
-      match: ({context}) => context.marker == 'number' && context.number == 'many', 
+      match: ({context}) => ['number', 'integer'].includes(context.marker) && context.number == 'many', 
       apply: ({context}) => `${context.value}'s` 
     },
     { 
       where: where(),
-      match: ({context}) => context.marker == 'number', 
+      match: ({context}) => ['number', 'integer'].includes(context.marker),
       apply: ({context}) => `${context.value}` 
     },
   ],
