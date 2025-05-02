439e09107e23b867d97a1d55062935ffba98d903
Greg McClement <greg.mcclement@gmail.com>
2025-05-02 11:47:12 -0700

    cleanup

--- kms/common/dimension.js
+++ kms/common/dimension.js
@@ -175,14 +175,6 @@ const config = {
       isA: ['number'],
     },
   ],
-  associations: {
-    positive: [
-      { context: [["what",0],["is",0],["the",0],["unit",0],["propertyOf",0],["dimension",0]], choose: 1 },
-      { context: [["what",0],["is",0],["the",0],["unit",1],["propertyOf",0],["dimension",1]], choose: 1 },
-      // { context: [["integer",0],["degree",0],["fahrenheit",0]], choose: 1 },
-    ],
-  }
-
 };
 
 const template = {
