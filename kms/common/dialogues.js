061cae592f570f0da2e880aae04e88691c2c659b
Greg McClement <greg.mcclement@gmail.com>
2025-05-02 10:57:52 -0700

    cleanup

--- kms/common/dialogues.js
+++ kms/common/dialogues.js
@@ -103,33 +103,10 @@ const config = {
   ],
   associations: {
     negative: [
-      // [['isEd', 0], ['unknown', 0]],
-      // [['isEd', 0], ['unknown', 1]],
-      // [['is', 0], ['means', 0]],
     ],
     positive: [
-      // [['is', 0], ['unknown', 1]],
-      // [['isEd', 0], ['means', 0]],
-      // [['unknown', 0], ['list', 0], ['unknown', 0]],
       { context: [['unknown', 0], ['isEdAble', 0]], choose: 1 },
-
-      { context: [['listable', 0], ['list', 0], ['listable', 0]], choose: 0 },
-      { context: [['the', 0], ['listable', 0], ['list', 0], ['listable', 0]], choose: 1 },
-      { context: [['the', 0], ['listable', 0], ['list', 0], ['the', 0], ['listable', 0]], choose: 1 },
-
-      { context: [['queryable', 0], ['is', 0], ['article', 0], ['theAble', 0]], choose: 1 },
-      { context: [['queryable', 1], ['is', 0], ['article', 0], ['theAble', 0]], choose: 1 },
-      { context: [['queryable', 1], ['is', 0], ['article', 0], ['theAble', 1]], choose: 1 },
-
       { context: [['isQuery', 0], ['a', 0], ['unknown', 0], ['a', 0], ['unknown', 0]], choose: 0 },
-
-      { context: [['isQuery', 0], ['unknown', 0], ['unknown', 0]], choose: 0 },
-
-      { context: [['isQuery', 0], ['unknown', 0], ['article', 0], ['unknown', 0]], choose: 0 },
-
-      { context: [['isQuery', 0], ['queryable', 0], ['article', 0], ['queryable', 0]], choose: 0 },
-      { context: [['isQuery', 0], ['queryable', 1], ['article', 0], ['queryable', 0]], choose: 0 },
-      { context: [['isQuery', 0], ['queryable', 1], ['article', 0], ['queryable', 1]], choose: 0 },
 
       { context: [["queryable",0],["is",0],["isEder",0],["isEdAble",0],["by",0]], choose: 1 },
       { context: [["queryable",1],["is",0],["isEder",0],["isEdAble",0],["by",0]], choose: 1 },
@@ -144,7 +121,6 @@ const config = {
 
       ...listorama('unknown'),
       ...listorama('queryable'),
-      // ...listorama('listable'),
     ]
   },
   bridges: [
