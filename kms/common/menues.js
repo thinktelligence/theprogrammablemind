d40b219348aaf37f03c69bd5281994f6a428f6f1
Greg McClement <greg.mcclement@gmail.com>
2025-05-12 09:58:15 -0700

    menus km

---
+++ kms/common/menues.js
@@ -0,0 +1,61 @@
+const { knowledgeModule, where, Digraph } = require('./runtime').theprogrammablemind
+const { defaultContextCheck } = require('./helpers')
+const ui = require('./ui')
+const menus_tests = require('./menus.test.json')
+const menus_instance = require('./menus.instance.json')
+
+class API {
+  initialize({ objects }) {
+    this._objects = objects
+  }
+
+  move(direction, steps = 1, units = undefined) {
+    this._objects.move = { direction, steps, units }
+  }
+
+  select(item) {
+    this._objects.select = { item }
+  }
+
+  unselect(item) {
+    this._objects.unselect = { item }
+  }
+
+  cancel(direction) {
+    this._objects.cancel = true
+  }
+
+  stop(action) {
+    this._objects.stop = action
+  }
+}
+
+const config = {
+  name: 'menus',
+};
+
+const template = {
+  fragments: [
+  ],
+}
+
+knowledgeModule({ 
+  config,
+  includes: [dialogues, ordinals, countable],
+  api: () => new API(),
+
+  module,
+  description: 'Control menues with speech',
+  test: {
+    name: './menus.test.json',
+    contents: menus_tests,
+    checks: {
+      objects: ['move', 'select', 'unselect', 'cancel', 'stop'],
+      context: defaultContextCheck(['operator', 'direction', 'moveable']),
+    },
+  },
+  template: {
+    template,
+    instance: menus_instance
+  }
+})
