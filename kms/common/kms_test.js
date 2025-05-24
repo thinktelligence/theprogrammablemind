99b41a2bd28fdc0e489e3149ac2733ddbcb27937
Greg McClement <greg.mcclement@gmail.com>
2025-05-24 09:52:28 -0700

    add test

--- kms/common/kms_test.js
+++ kms/common/kms_test.js
@@ -19,5 +19,20 @@ describe('helpers', () => {
     await km.setApi(() => api)
     expect(km.config.objects.associations).toStrictEqual(["menus"])
   })
+
+  it('sdefault setup lands in right place', async () => {
+    const km = await menus()
+    class API extends km.api.constructor {
+    }
+    const api = new API()
+    km.stop_auto_rebuild()
+      await km.setApi(() => api)
+      try {
+        km.api.addMenu('file')
+        expect(true).toBe(false)
+      } catch( e ) {
+        expect(e.toString()).toBe('Error: The API cannot be accessed until the auto rebuild is restarted')
+      }
+  })
 })
 
