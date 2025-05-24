b18819e3aa401be81af85749c54adad8930fe63a
Greg McClement <greg.mcclement@gmail.com>
2025-05-24 08:48:53 -0700

    add test + fix test

--- kms/common/kms_test.js
+++ kms/common/kms_test.js
@@ -1,14 +1,23 @@
 const menus = require('./menus')
 
 describe('helpers', () => {
-  it('NEO23 setting api runs initialize', async () => {
+  it('NEOS23 setting api runs initialize', async () => {
     const km = await menus()
     class API extends km.api.constructor {
     }
     const api = new API()
-    km.setApi(() => api)
+    await km.setApi(() => api)
     expect(api._config).not.toBeNull()
     expect(api._objects).not.toBeNull()
+  })
+
+  it('NEO23 sdefault association set', async () => {
+    const km = await menus()
+    class API extends km.api.constructor {
+    }
+    const api = new API()
+    await km.setApi(() => api)
+    expect(km.config.objects.associations).toStrictEqual(["menus"])
   })
 })
 
