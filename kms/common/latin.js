e4045c5803f220447e0ea21948571dcb1254a4cd
Greg McClement <greg.mcclement@gmail.com>
2025-09-08 13:06:26 -0700

    scriptum latinum

---
+++ kms/common/latin.js
@@ -0,0 +1,72 @@
+const { knowledgeModule, where } = require('./runtime').theprogrammablemind
+const { defaultContextCheck } = require('./helpers')
+const gdefaults = require("./gdefaults")
+const latin_tests = require('./latin.test.json')
+
+/*
+  marcus est vir
+  marcus vir est
+
+  estne
+  marcus quintus iuliaque
+*/
+const config = {
+  name: 'latin',
+  operators: [
+    "((*) [queryMarker])",
+    "([hierarchiable])",
+    "((hierarchiable) [hierarchy|] (hierarchiable))",
+    "((hierarchiable) (hierarchiable) [hierarchy|])",
+    "([hierarchy|] (hierarchiable) (hierarchiable))",
+  ],
+  bridges: [
+    { 
+      id: "queryMarker",
+      bridge: "{ ...next(before[0]), question: true }",
+      before: ['hierarchy'],
+    },
+    { id: "hierarchiable" },
+    { 
+      id: "hierarchy",
+      localHierarchy: [['unknown', 'hierarchiable']],
+      bridge:  "{ ...next(operator), child: arguments[0], parent: arguments[1], question: less_than(indexes.operator, indexes.arguments[0]), interpolate: all }",
+      words: [
+        { word: 'sum', number: 'singular', person: 'first' },
+        { word: 'es', number: 'singular', person: 'second' },
+        { word: 'est', number: 'singular', person: 'third' },
+        { word: 'sumus', number: 'plural', person: 'first' },
+        { word: 'estis', number: 'plural', person: 'second' },
+        { word: 'sunt', number: 'plural', person: 'third' },
+      ]
+    },
+  ],
+  words: {
+    literals: {
+      "ne": [{ id: 'queryMarker', initial: { value: 'queryMarker' } }],
+    },
+    patterns: [
+      { "pattern": ["ne"], defs: [
+        {
+          id: "queryMarker", 
+          uuid: '1', 
+          initial: { value: 'greg' } 
+        }
+      ]},
+    ],
+  },
+};
+
+knowledgeModule( { 
+  config,
+  includes: [gdefaults],
+
+  module,
+  description: 'latin',
+  test: {
+    name: './latin.test.json',
+    contents: latin_tests,
+    checks: {
+      context: [defaultContextCheck()],
+    },
+  },
+})
